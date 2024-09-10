c6559d8cac780fb7bd7e171350ea9987a0ead246
Cesar <cesar.santiago@utp.ac.pa>
2024-09-09 19:51:55 -0700

    Laboratorio 4 finalizado

---
+++ Laboratorio4/Laboratorio44/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Laboratorio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del estudiante:");
+            float score = float.Parse(Console.ReadLine()); 
+
+            if (score >= 70)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score}... --- Usted ha aprobado");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score}... --- Usted ha reprobado, debe repetir");
+            }
+        }
+    }
+}
