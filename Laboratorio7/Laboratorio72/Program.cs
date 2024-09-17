569cfd725b7278e68fa47995261d35150770cca6
Cesar <cesar.santiago@utp.ac.pa>
2024-09-16 19:44:28 -0700

    Laboratorios 71 y 72 completados

---
+++ Laboratorio7/Laboratorio72/Program.cs
@@ -0,0 +1,13 @@
+﻿using Laboratorio72;
+
+namespace Laboratorio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        }
+    }
+}
